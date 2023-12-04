d6c7cc577c98e26d131ce003fee80c6e2e41efb5
gryzun33 <yakushevao777@gmail.com>
2023-12-04 21:01:10 +0300

    feat: add switching categories of menu

--- coffee-house/pages/menu/menu.js
+++ coffee-house/pages/menu/menu.js
@@ -28,17 +28,131 @@ function toggleBurger() {
   }
 }
 
+// menu
+const menuTabs = document.querySelectorAll(`[name='menu']`);
+const menuWrapper = document.querySelector('.menu__wrapper');
+const loadMoreBtn = document.querySelector('.refresh-btn');
+// const modalWrapper = document.querySelector('.modal-wrapper');
+let lengthOfMenu = 0;
+
+async function getData(category) {
+  const res = await fetch('./products.json');
+  const allProducts = await res.json();
+  const products = allProducts.filter(
+    (prod) => prod.category === `${category}`
+  );
+  renderMenu(products);
+  renderLoadMoreBtn(products.length);
+}
+
+// async function getProductData(productName) {
+//   const res = await fetch('./products.json');
+//   const allProducts = await res.json();
+//   const product = allProducts.find((prod) => prod.name === `${productName}`);
+//   createModal(modalWrapper, product);
+// }
+
+function renderMenu(products) {
+  lengthOfMenu = 0;
+  const menuList = document.querySelector('.menu__items');
+  menuList.innerHTML = '';
+  products.forEach((item, i) => {
+    const menuItem = document.createElement('li');
+    menuItem.classList.add('menu-item');
+    if (window.innerWidth < 1090 && i > 3) {
+      menuItem.classList.add('hidden');
+    }
+    menuItem.innerHTML = `
+      <div class="menu-item__image-wrapper">
+        <img src=${item.image} alt=${item.name}>
+      </div>
+      <div class="menu-item__description">
+        <div class="menu-item__content">
+          <h3 class="menu-item__title">${item.name}</h3>
+          <p class="menu-item__text">${item.description}</p>
+        </div>
+        <p class="menu-item__price">$${item.price}</p>
+      </div>
+    `;
+    // menuItem.addEventListener('click', () => {
+    //   modalWrapper.classList.remove('modal-hidden');
+    //   modalWrapper.classList.add('modal-show');
+    //   document.body.style.overflowY = 'hidden';
+    //   getProductData(item.name);
+    // });
+    menuList.append(menuItem);
+    lengthOfMenu += 1;
+  });
+}
+
+function updateMenuView(length) {
+  const menuItems = document.querySelectorAll('.menu-item');
+  const items = [...menuItems].filter((el, i) => i > 3);
+  if (window.innerWidth >= 1090 && length > 4) {
+    items.forEach((el) => {
+      el.classList.remove('hidden');
+    });
+  }
+  if (window.innerWidth < 1090 && length > 4) {
+    items.forEach((el) => {
+      el.classList.add('hidden');
+    });
+  }
+}
+
+function renderLoadMoreBtn(length) {
+  if (window.innerWidth >= 1090) {
+    loadMoreBtn.classList.remove('visible');
+    return;
+  }
+  if (length <= 4) {
+    loadMoreBtn.classList.remove('visible');
+    return;
+  } else {
+    loadMoreBtn.classList.add('visible');
+  }
+}
+
+menuTabs.forEach((input) => {
+  input.addEventListener('input', () => {
+    getData(input.id);
+  });
+});
+
+loadMoreBtn.addEventListener('click', () => {
+  const menuItems = document.querySelectorAll('.menu-item');
+  const hiddenItems = [...menuItems].filter((el, i) => i > 3);
+  hiddenItems.forEach((item) => {
+    item.classList.remove('hidden');
+  });
+  loadMoreBtn.classList.remove('visible');
+});
+
+getData('coffee');
+
+// resize
 let lastWindowWidth = window.innerWidth;
 let newWindowWidth;
 
 window.addEventListener('resize', () => {
   newWindowWidth = window.innerWidth;
+
   if (
     lastWindowWidth <= 768 &&
     newWindowWidth >= 769 &&
-    burgerMenu.classList.contains('navigation-menu-show')
+    burgerMenu.classList.contains('burger-menu-show')
   ) {
     toggleBurger();
   }
+
+  if (newWindowWidth < 1090 && lastWindowWidth >= 1090) {
+    renderLoadMoreBtn(lengthOfMenu);
+    updateMenuView(lengthOfMenu);
+  }
+  if (newWindowWidth >= 1090 && lastWindowWidth < 1090) {
+    renderLoadMoreBtn(lengthOfMenu);
+    updateMenuView(lengthOfMenu);
+  }
+
   lastWindowWidth = newWindowWidth;
 });
