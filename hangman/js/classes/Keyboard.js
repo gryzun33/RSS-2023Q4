ddcca1c1bcf09a2360151b4276cbbd0793bcc4fb
gryzun33 <yakushevao777@gmail.com>
2024-01-04 14:45:37 +0300

    feat: add method to keyboard

--- hangman/js/classes/Keyboard.js
+++ hangman/js/classes/Keyboard.js
@@ -4,7 +4,7 @@ export class Keyboard {
 
   constructor(parent) {
     this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    this.keyButtons = {};
+    this.keyButtons = [];
     this.createView(parent);
   }
 
@@ -12,11 +12,18 @@ export class Keyboard {
     
     for(let i = 0; i < this.alphabet.length; i+=1) {
       let keyLetter = this.alphabet[i];
-      let keyElem = createHTMLElement('button', 'key', par);
-      keyElem.innerText = keyLetter;
+      let keyBtn = createHTMLElement('button', 'key', par);
+      keyBtn.innerText = keyLetter;
+      this.keyButtons.push(keyBtn);
       // keyElem.dataset.letter = keyLetter;
-      this.keyButtons[keyLetter] = keyElem;
+      // this.keyButtons[keyLetter] = keyElem;
     }
+  }
+
+  updateView() {
+    this.keyButtons.forEach((btn) => {
+      btn.disabled = false;
+    }) 
   }
 
 
