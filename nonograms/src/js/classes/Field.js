a21899c091a785edb95a667ee0e22fb1f87d36f3
gryzun33 <yakushevao777@gmail.com>
2024-01-22 13:19:20 +0300

    feat: add styles to field

--- nonograms/src/js/classes/Field.js
+++ nonograms/src/js/classes/Field.js
@@ -17,20 +17,24 @@ export default class Field {
     const rowFirst = createHTMLElement('tr', 'row-first', fullField);
     const rowSecond = createHTMLElement('tr', 'row-second', fullField);
     const emptyField = createHTMLElement('td', 'empty-field', rowFirst);
+    // const emptyBox = createHTMLElement('div', 'empty-box', emptyField);
+
+    // const emptyTable = createHTMLElement('table', 'empty-table', emptyField);
+    // const emptyCell = createHTMLElement('table', 'empty-table', emptyField);
     const topHintsBigCell = createHTMLElement('td', 'top-hints', rowFirst);
     const topHintsTable = createHTMLElement('table', 'top-hints-table', topHintsBigCell);
     const leftHintsBigCell = createHTMLElement('td', 'left-hints', rowSecond);
-    const leftHintsTable = createHTMLElement('table', 'row-hints-table', leftHintsBigCell);
+    const leftHintsTable = createHTMLElement('table', 'left-hints-table', leftHintsBigCell);
 
     const mainFieldWrapper = createHTMLElement('td', 'main-field-wrapper', rowSecond);
-    this.mainField = createHTMLElement('table', 'main-field', mainFieldWrapper);
+    this.mainFieldTable = createHTMLElement('table', 'main-field-table', mainFieldWrapper);
 
     // render top hints
     for (let j = 0; j < this.topHints[0].length; j += 1) {
       const rowTopHints = createHTMLElement('tr', 'row-top-hints', topHintsTable);
       for (let i = 0; i < this.topHints.length; i += 1) {
-        const text = this.topHints[i][j] ? this.topHints[i][j] : 0;
-        const rowTopHint = createHTMLElement('td', 'row-top-hint', rowTopHints, text);
+        const text = this.topHints[i][j] ? this.topHints[i][j] : '';
+        const rowTopHint = createHTMLElement('td', 'row-top-hint cell', rowTopHints, text);
       }
     }
 
@@ -39,17 +43,17 @@ export default class Field {
       const rowLeftHints = createHTMLElement('tr', 'row-left-hints', leftHintsTable);
 
       for (let j = 0; j < this.leftHints[0].length; j += 1) {
-        const text = this.leftHints[i][j] ? this.leftHints[i][j] : 0;
-        const rowLeftHint = createHTMLElement('td', 'row-left-hint', rowLeftHints, text);
+        const text = this.leftHints[i][j] ? this.leftHints[i][j] : '';
+        const rowLeftHint = createHTMLElement('td', 'row-left-hint cell', rowLeftHints, text);
       }
     }
 
     // render mainField
     for (let i = 0; i < this.game.gameMatrix.length; i += 1) {
       const userGameRow = [];
-      const rowField = createHTMLElement('tr', 'field-row', this.mainField);
+      const rowField = createHTMLElement('tr', 'field-row', this.mainFieldTable);
       for (let j = 0; j < this.game.gameMatrix.length; j += 1) {
-        const cellField = createHTMLElement('td', 'field-cell', rowField);
+        const cellField = createHTMLElement('td', 'field-cell cell', rowField);
         const cell = [0];
         userGameRow.push(cell);
       }
