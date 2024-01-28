669056d0e7eea298792c1cd59a4f9da475ad512e
gryzun33 <yakushevao777@gmail.com>
2024-01-28 13:43:21 +0300

    feat: add sourcemap to vite config

--- nonograms/vite.config.js
+++ nonograms/vite.config.js
@@ -2,5 +2,8 @@ import { defineConfig } from 'vite';
 import eslintPlugin from 'vite-plugin-eslint';
 
 export default defineConfig({
+  build: {
+    sourcemap: true,
+  },
   plugins: [eslintPlugin()],
 });
