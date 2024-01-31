ebb0927615af38d41e18ece3c817cc203d376449
gryzun33 <yakushevao777@gmail.com>
2024-01-31 19:14:38 +0300

    refactor: delete comments in scss

--- nonograms/src/index.js
+++ nonograms/src/index.js
@@ -1,13 +1,6 @@
-// import './index.html';
 import './style.scss';
 
 import Main from './js/classes/Main';
-
-// function sum(a, b) {
-//   return a + b;
-// }
-
-// sum(3, 4);
 
 class App {
   constructor() {
@@ -16,7 +9,6 @@ class App {
 
   createView() {
     this.main = new Main(document.body);
-    this.main.getHTMLElement();
   }
 }
 /* eslint no-unused-vars: "off" */
