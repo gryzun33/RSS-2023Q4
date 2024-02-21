3d03663404e0c6b7a0e6048ce67e4a82626d36fc
gryzun33 <yakushevao777@gmail.com>
2024-02-21 22:12:11 +0300

    feat: add eslint plugin to webpackconfig

--- news-api/src/components/app/app.ts
+++ news-api/src/components/app/app.ts
@@ -2,49 +2,52 @@ import AppController from '../controller/controller';
 import { AppView } from '../view/appView';
 
 type Source = {
-    category:string,
-    country:string,
-    description:string,
-    id:string,
-    language:string,
-    name:string,
-    url:string
-}
+  category: string;
+  country: string;
+  description: string;
+  id: string;
+  language: string;
+  name: string;
+  url: string;
+};
 
 type SourceData = {
-  sources:Source[],
-  status:string;  
-}
+  sources: Source[];
+  status: string;
+};
 
 type Options = {
-    [key: string]: string;
-}
+  [key: string]: string;
+};
 
 type Article = {
-    [key: string]: string|Options;
- }
- 
- 
- type NewsList = {
-     status:string,
-     totalResults: number,
-     articles:Article[] 
- }
+  [key: string]: string | Options;
+};
+
+type NewsList = {
+  status: string;
+  totalResults: number;
+  articles: Article[];
+};
+
+// type DrawFunction = (value: SourceData | NewsList) => void;
 
 class App {
-    controller:AppController;
-    view:AppView;
+  controller: AppController;
+  view: AppView;
 
-    constructor() {
-        this.controller = new AppController();
-        this.view = new AppView();
-    }
+  constructor() {
+    this.controller = new AppController();
+    this.view = new AppView();
+  }
 
-    start() {
-        const sourcesElem = document.querySelector('.sources') as HTMLElement;
-        sourcesElem.addEventListener('click', (e) => this.controller.getNews(e, (data:NewsList) => this.view.drawNews(data)));
-        this.controller.getSources((data:SourceData) => this.view.drawSources(data));
-    }
+  start() {
+    const sourcesElem = document.querySelector('.sources') as HTMLElement;
+    sourcesElem.addEventListener('click', (e) =>
+      this.controller.getNews(e, (data: NewsList) => this.view.drawNews(data))
+    );
+    this.controller.getSources((data: SourceData) => this.view.drawSources(data));
+  }
 }
 
 export default App;
--- news-api/src/components/controller/loader.ts
+++ news-api/src/components/controller/loader.ts
@@ -1,72 +1,89 @@
 type Options = {
-    [key: string]: string;
-}
+  [key: string]: string;
+};
 
 type Article = {
-   [key: string]: string|Options;
-}
-
+  [key: string]: string | Options;
+};
 
 type NewsList = {
-    status:string,
-    totalResults: number,
-    articles:Article[] 
-}
+  status: string;
+  totalResults: number;
+  articles: Article[];
+};
 
+type Source = {
+  category: string;
+  country: string;
+  description: string;
+  id: string;
+  language: string;
+  name: string;
+  url: string;
+};
+
+type SourceData = {
+  sources: Source[];
+  status: string;
+};
+type DrawFunction = (value: SourceData | NewsList) => void;
 
 interface ILoader {
-    baseLink:string,
-    options:Options,
- }
+  baseLink: string;
+  options: Options;
+}
 
 class Loader implements ILoader {
-    baseLink: string;
-    options: Options;
+  baseLink: string;
+  options: Options;
 
-    constructor(baseLink:string, options:Options) {
-        this.baseLink = baseLink;
-        this.options = options;
+  constructor(baseLink: string, options: Options) {
+    this.baseLink = baseLink;
+    this.options = options;
+  }
+
+  public getResp(
+    { endpoint, options = {} }: { endpoint: string; options?: Options },
+    callback = () => {
+      console.error('No callback for GET response');
+    }
+  ): void {
+    console.log('endpoint= ', endpoint);
+    this.load('GET', endpoint, callback, options);
+  }
+
+  private errorHandler(res: Response): Response {
+    console.log('res=', res);
+    if (!res.ok) {
+      if (res.status === 401 || res.status === 404)
+        console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
+      throw Error(res.statusText);
     }
 
-    public getResp(
-        { endpoint, options = {} }:{ endpoint: string, options?: Options },
-        callback = () => {
-            console.error('No callback for GET response');
-        }
-    ) { 
-        console.log('endpoint= ', endpoint);
-        this.load('GET', endpoint, callback, options);
-    }
+    return res;
+  }
 
-    private errorHandler(res:Response):Response {
-        console.log('res=', res);
-        if (!res.ok) {
-            if (res.status === 401 || res.status === 404)
-                console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
-            throw Error(res.statusText);
-        }
+  private makeUrl(options: Options, endpoint: string): string {
+    const urlOptions: Options = { ...this.options, ...options };
+    // console.log('thisoptions=', this.options);
+    // console.log('options=', options);
+    // console.log('urlOptions=', urlOptions);
+    let url = `${this.baseLink}${endpoint}?`;
 
-        return res;
-    }
+    Object.keys(urlOptions).forEach((key) => {
+      url += `${key}=${urlOptions[key]}&`;
+    });
 
-    private makeUrl(options:any, endpoint:string) {
-        const urlOptions = { ...this.options, ...options };
-        let url = `${this.baseLink}${endpoint}?`;
+    return url.slice(0, -1);
+  }
 
-        Object.keys(urlOptions).forEach((key) => {
-            url += `${key}=${urlOptions[key]}&`;
-        });
-
-        return url.slice(0, -1);
-    }
-
-    private load(method:string, endpoint:string, callback:any, options = {}):void {
-        fetch(this.makeUrl(options, endpoint), { method })
-            .then(this.errorHandler)
-            .then((res:Response) => res.json())
-            .then((data:NewsList) => callback(data))
-            .catch((err) => console.error(err));
-    }
+  private load(method: string, endpoint: string, callback: DrawFunction, options: Options = {}): void {
+    fetch(this.makeUrl(options, endpoint), { method })
+      .then(this.errorHandler)
+      .then((res: Response) => res.json())
+      .then((data: NewsList) => callback(data))
+      .catch((err: Error) => console.error(err));
+  }
 }
 
 export default Loader;
