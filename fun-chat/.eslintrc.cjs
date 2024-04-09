fe5a0f36fd4dfaf251f7195324cec3802058f955
gryzun33 <yakushevao777@gmail.com>
2024-04-09 12:58:27 +0300

    chore: add eslint

---
+++ fun-chat/.eslintrc.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  plugins: ['@typescript-eslint'],
+  extends: ['airbnb-base', 'airbnb-typescript/base', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  rules: {
+    '@typescript-eslint/no-explicit-any': 2,
+    'no-console': 'off',
+    'lines-between-class-members': 'off',
+    '@typescript-eslint/lines-between-class-members': 'off',
+    'class-methods-use-this': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+  },
+};
