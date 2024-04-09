7077728b9b873ea523ad7129429f42dc4be1dc09
gryzun33 <yakushevao777@gmail.com>
2024-04-09 14:15:19 +0300

    chore: add configuration vite

--- fun-chat/vite.config.js
+++ fun-chat/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite';
 import eslintPlugin from 'vite-plugin-eslint';
+import path from 'node:path';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@assets': path.resolve(import.meta.dirname, 'src', 'assets'),
+    },
+  },
   base: './',
   build: {
     sourcemap: true,
