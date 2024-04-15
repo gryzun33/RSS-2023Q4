61a2b64b382dd404ff7b609817d257045a28d31e
gryzun33 <yakushevao777@gmail.com>
2024-04-15 09:54:03 +0300

    fix: fix styles

--- fun-chat/src/app/components/View/NewMessage.ts
+++ fun-chat/src/app/components/View/NewMessage.ts
@@ -1,7 +1,22 @@
 import BaseComponent from './BaseComponent';
+import Button from './Button';
 import { Props } from '../../utils/types';
 
 export default class newMessage extends BaseComponent {
+  protected messageForm = new BaseComponent<HTMLFormElement>({
+    tag: 'form',
+    classNames: ['message-form'],
+  });
+  protected messageInput = new BaseComponent<HTMLInputElement>({
+    tag: 'input',
+    classNames: ['message-input'],
+  });
+
+  protected loginBtn = new Button({
+    type: 'submit',
+    classNames: ['send-btn'],
+    text: 'Send',
+  });
   constructor(props: Props) {
     super(props);
     this.createView();
