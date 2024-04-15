c27844f92a591eaf74dcbcbf492c77e00fb05486
gryzun33 <yakushevao777@gmail.com>
2024-04-15 15:57:36 +0300

    feat: add reconnect

--- fun-chat/src/app/components/WebSocketManager.ts
+++ fun-chat/src/app/components/WebSocketManager.ts
@@ -2,26 +2,122 @@
 
 type Handler = (arg: string) => void;
 
+// export default class WebSocketManager {
+//   private ws = new WebSocket('ws://localhost:4000');
+//   constructor(
+//     public dataHandler: Handler,
+//     public checkAuthorized: () => void
+//   ) {
+//     this.ws.addEventListener('open', this.onOpen);
+//   }
+
+//   onOpen = () => {
+//     console.log('connected');
+//     this.ws.addEventListener('message', this.onMessage);
+//     this.ws.addEventListener('close', this.onClose);
+//     this.ws.addEventListener('error', this.onError);
+
+//     this.checkAuthorized();
+//   };
+
+//   onClose = () => {
+//     console.log('disconnected');
+//   };
+
+//   onMessage = (event: MessageEvent) => {
+//     const { data } = event;
+//     this.dataHandler(data);
+//     console.log('Message from server:', event.data);
+//   };
+
+//   onError = (event: Event) => {
+//     console.error('WebSocket error:', event);
+//   };
+//   send(message: string) {
+//     console.log('send', message);
+//     this.ws.send(message);
+//   }
+// }
+
+// export default class WebSocketManager {
+//   private ws = new WebSocket('ws://localhost:4000');
+//   protected reconnectInterval: number = 1000;
+//   protected reconnectIntervalId?: number;
+//   constructor(
+//     public dataHandler: Handler,
+//     public checkAuthorized: () => void
+//   ) {
+//     this.ws.addEventListener('open', this.onOpen);
+//     this.ws.addEventListener('message', this.onMessage);
+//     this.ws.addEventListener('close', this.onClose);
+//     this.ws.addEventListener('error', this.onError);
+//   }
+
+//   onOpen = () => {
+//     console.log('connected');
+//     console.log('readystate=', this.ws.readyState);
+//     if (this.ws.readyState !== WebSocket.OPEN) {
+//       return;
+//     }
+//     clearInterval(this.reconnectIntervalId);
+//     console.log('clearinterval');
+//     this.checkAuthorized();
+//   };
+
+//   onClose = () => {
+//     console.log('disconnected');
+//     this.reconnect();
+//   };
+
+//   onMessage = (event: MessageEvent) => {
+//     const { data } = event;
+//     this.dataHandler(data);
+//     console.log('Message from server:', event.data);
+//   };
+
+//   onError = (event: Event) => {
+//     console.error('WebSocket error:', event);
+//     // this.onClose();
+//   };
+//   send(message: string) {
+//     console.log('send', message);
+//     this.ws.send(message);
+//   }
+
+//   reconnect = () => {
+//     this.reconnectIntervalId = setInterval(() => {
+//       this.ws = new WebSocket('ws://localhost:4000');
+//       console.log('readystate=', this.ws.readyState);
+//     }, this.reconnectInterval);
+//   };
+// }
+
 export default class WebSocketManager {
-  private ws = new WebSocket('ws://localhost:4000');
+  private ws?: WebSocket;
+  protected reconnectInterval: number = 1000;
   constructor(
     public dataHandler: Handler,
     public checkAuthorized: () => void
   ) {
-    this.ws.addEventListener('open', this.onOpen);
+    this.connect();
   }
 
-  onOpen = () => {
-    console.log('connected');
+  connect = () => {
+    this.ws = new WebSocket('ws://localhost:4000');
+    this.ws.addEventListener('open', this.onOpen);
     this.ws.addEventListener('message', this.onMessage);
     this.ws.addEventListener('close', this.onClose);
     this.ws.addEventListener('error', this.onError);
+  };
 
+  onOpen = () => {
     this.checkAuthorized();
   };
 
   onClose = () => {
-    console.log('disconnected');
+    setTimeout(() => {
+      this.connect();
+    }, this.reconnectInterval);
   };
 
   onMessage = (event: MessageEvent) => {
@@ -32,9 +128,10 @@ export default class WebSocketManager {
 
   onError = (event: Event) => {
     console.error('WebSocket error:', event);
+    this.ws?.close();
   };
   send(message: string) {
     console.log('send', message);
-    this.ws.send(message);
+    this.ws?.send(message);
   }
 }
